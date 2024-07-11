be296572ef0d20811de45021068c5918b9b7603f
Jimmy <jabez007@users.noreply.github.com>
2024-07-11 14:28:57 -0500

    different way to promisify question

--- src/ask-question.js
+++ src/ask-question.js
@@ -1,25 +1,28 @@
 import * as readline from 'readline'
-import { promisify } from 'util'
+
+function question (inquiry) {
+  return new Promise(resolve => {
+    const ui = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    })
+
+    ui.question(inquiry, input => {
+      ui.close()
+      resolve(input)
+    })
+  })
+}
 
 export default async (query, preset = '', validate = () => true) => {
-  const ui = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
-
-  const question = promisify(ui.question.bind(ui))
-
   let resp
   while (true) {
     resp = (await question(query)) || preset
-    console.log(`resp: ${resp}`)
     const valid = validate(resp)
-    console.log(`valid: ${valid}`)
     if (valid === true) {
       break
     }
     console.log(valid || `Invalid response: ${resp}`)
   }
-  ui.close()
   return resp
 }
