576d17005b69a58bffb244c1e11b445210da2bd5
Jimmy <jabez007@users.noreply.github.com>
2024-07-10 19:25:42 -0500

    make a new directory and move into it

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 #!/usr/bin/env node
-
-import * as childProcess from 'child_process'
 import { promisify } from 'util'
+import { join } from 'path'
+import * as childProcess from 'child_process'
+import * as fs from 'fs'
 
 const exec = promisify(childProcess.exec)
+const mkdir = promisify(fs.mkdir)
+
+/*
+ * make a new directory and move into it
+ */
+const defaultFolderName = "my-node-app";
+const initWorkingDirectory = process.cwd();
+
+let folderName = defaultFolderName;
+if (process.argv.slice(2).length > 0) {
+  folderName = process.argv.slice(2)[0];
+}
+console.log(`creating directory ${folderName}`);
+fs.mkdirSync(join(initWorkingDirectory, folderName));
+
+process.chdir(join(initWorkingDirectory, folderName));
+/* END */
 
 async function main() {
-    console.log(process.argv);
-
-    console.log('npm init')
-    await exec('npm init --yes')
+    console.log('npm init');
+    await exec('npm init --yes');
 }
 
 main()
