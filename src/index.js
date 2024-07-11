ff3ec12f059626961c6ff41a6bb94cb39a3c505e
Jimmy <jabez007@users.noreply.github.com>
2024-07-11 10:45:23 -0500

    I should use ESLint myself

--- src/index.js
+++ src/index.js
@@ -4,67 +4,67 @@ import { join } from 'path'
 import * as childProcess from 'child_process'
 import * as fs from 'fs'
 
-const exec = promisify(childProcess.exec);
-const mkdir = promisify(fs.mkdir);
-const writeFile = promisify(fs.writeFile);
+const exec = promisify(childProcess.exec)
+const mkdir = promisify(fs.mkdir)
+const writeFile = promisify(fs.writeFile)
 
 /*
  * calculate project directory name
  */
-const defaultFolderName = "my-node-app";
-const initWorkingDirectory = process.cwd();
+const defaultFolderName = 'my-node-app'
+const initWorkingDirectory = process.cwd()
 
-let folderName = defaultFolderName;
+let folderName = defaultFolderName
 if (process.argv.slice(2).length > 0) {
-  folderName = process.argv.slice(2)[0];
+  folderName = process.argv.slice(2)[0]
 }
-const projectWorkingDirectory = join(initWorkingDirectory, folderName);
+const projectWorkingDirectory = join(initWorkingDirectory, folderName)
 /* END */
 
-async function main() {
+async function main () {
   /*
    * make new directory and move into it
    */
-  console.log(`creating directory ${folderName}`);
-  await mkdir(projectWorkingDirectory);
-  process.chdir(projectWorkingDirectory);
+  console.log(`creating directory ${folderName}`)
+  await mkdir(projectWorkingDirectory)
+  process.chdir(projectWorkingDirectory)
   /* END */
 
-  /* 
+  /*
    * initialize npm in new project directory
    */
-  console.log('npm init');
-  await exec('npm init --yes');
+  console.log('npm init')
+  await exec('npm init --yes')
   /* END */
 
   /*
    * create and set package main
    */
-  console.log('creating src directory');
-  await mkdir(join(projectWorkingDirectory, 'src'));
+  console.log('creating src directory')
+  await mkdir(join(projectWorkingDirectory, 'src'))
 
-  console.log('creating index.js file');
-  fs.closeSync(fs.openSync(join(projectWorkingDirectory, 'src', 'index.js'), 'w'));
+  console.log('creating index.js file')
+  fs.closeSync(fs.openSync(join(projectWorkingDirectory, 'src', 'index.js'), 'w'))
 
-  console.log('updating main in package.json');
-  await exec("npm pkg set main=./src/index.js");
+  console.log('updating main in package.json')
+  await exec('npm pkg set main=./src/index.js')
   /* END */
 
   /*
    * set up ESLint
    */
-  //console.log('installing ESLint (this may take a while)');
-  //await exec("npm install --save-dev eslint @eslint/js");
-  console.log('installing Standard ESLint config (this may take a while)');
-  await exec("npm install --save-dev eslint-config-standard eslint-plugin-promise eslint-plugin-import eslint-plugin-n");
+  // console.log('installing ESLint (this may take a while)');
+  // await exec("npm install --save-dev eslint @eslint/js");
+  console.log('installing Standard ESLint config (this may take a while)')
+  await exec('npm install --save-dev eslint-config-standard eslint-plugin-promise eslint-plugin-import eslint-plugin-n')
 
-  console.log('writing .eslintrc file');
+  console.log('writing .eslintrc file')
   await writeFile(join(projectWorkingDirectory, '.eslintrc'), `{
   "extends": "standard"
 }`)
 
-  console.log('adding lint to scripts in package.json');
-  await exec('npm pkg set scripts.lint="eslint --fix ./src/**/*.{js,jsx,ts}"');
+  console.log('adding lint to scripts in package.json')
+  await exec('npm pkg set scripts.lint="eslint --fix ./src/**/*.{js,jsx,ts}"')
   /* END */
 }
 
